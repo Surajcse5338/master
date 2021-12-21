14792a0be97747ba030edba55eb46b4ea29ec662
Suraj Shinde <surajshinde231@gmail.com>
2021-12-21 21:25:38 +0530

    first commit.

---
+++ src/main/java/com/manage/builder/manageBuilderExpense/ManageBuilderExpenseApplication.java
@@ -0,0 +1,13 @@
+package com.manage.builder.manageBuilderExpense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageBuilderExpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageBuilderExpenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manage/builder/manageBuilderExpense/ManageBuilderExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manage.builder.manageBuilderExpense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageBuilderExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
